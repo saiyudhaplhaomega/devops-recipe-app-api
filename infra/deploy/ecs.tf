10007c264b2c496360d0fa0edbf5446090df9bf0
Saiyudh Mannan <saiyudh.mannan@gmail.com>
2025-09-04 13:22:10 +0200

    added cloudwatch log group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -31,7 +31,9 @@ resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   role       = aws_iam_role.app_task.name
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
-
+resource "aws_cloudwatch_log_group" "ecs_task_logs" {
+  name = "${local.prefix}-api"
+}
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
