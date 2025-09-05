2bb6455c899ae618de178a8df582900fd7bc7e5f
Saiyudh Mannan <saiyudh.mannan@gmail.com>
2025-09-05 22:23:54 +0200

    update ecs task to use efs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -86,6 +86,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = false
             containerPath = "/vol/web/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = false
+            containerPath = "/vol/web/media"
+            sourceVolume  = "efs-media"
           }
         ],
         logConfiguration = {
@@ -120,6 +125,11 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = true
             containerPath = "/vol/static"
             sourceVolume  = "static"
+          },
+          {
+            readOnly      = true
+            containerPath = "/vol/media"
+            sourceVolume  = "efs-media"
           }
         ]
         logConfiguration = {
@@ -137,7 +147,18 @@ resource "aws_ecs_task_definition" "api" {
   volume {
     name = "static"
   }
+  volume {
+    name = "efs-media"
+    efs_volume_configuration {
+      file_system_id     = aws_efs_file_system.media.id
+      transit_encryption = "ENABLED"
 
+      authorization_config {
+        access_point_id = aws_efs_access_point.media.id
+        iam             = "DISABLED"
+      }
+    }
+  }
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -23,3 +23,14 @@ resource "aws_security_group" "efs" {
     ]
   }
 }
+resource "aws_efs_access_point" "media" {
+  file_system_id = aws_efs_file_system.media.id
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101
+      permissions = "755"
+    }
+  }
+}
